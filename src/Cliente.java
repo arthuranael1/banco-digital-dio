1a02b6ae93cfcba535933ea8944b0d17dabbecbb
CusattiAndy <andyaacm@gmail.com>
2024-07-03 17:57:28 -0300

    Desafio Dio Cuentas Bancarias con procesos extras

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,10 @@
+import java.util.Date;
 
 public class Cliente {
 
 	private String nome;
+	private Date fechaCreacion;
+	private int id;
 
 	public String getNome() {
 		return nome;
@@ -9,6 +12,28 @@ public class Cliente {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void llenarFormularioAutomaticamente(Personas persona) {
+		nome = persona.getNombre();
+		id = persona.getId();
+		fechaCreacion = new Date(System.currentTimeMillis());
 	}
 
 }
