aac78a5c189e934dcc7a4e8b6766bf39afbd6645
jowgaze <jonatha.biomon.hp@gmail.com>
2024-07-13 00:44:22 -0300

    Feat: novos metodos em banco e atualização de main

--- src/Banco.java
+++ src/Banco.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
 	private String nome;
 	private List<Conta> contas;
+
+	public Banco(String nome) {
+		this.nome = nome;
+		this.contas = new ArrayList<>();
+	}
 
 	public String getNome() {
 		return nome;
@@ -19,6 +25,32 @@ public class Banco {
 
 	public void setContas(List<Conta> contas) {
 		this.contas = contas;
+	}
+
+	public void adicionarConta(Conta c) {
+		contas.add(c);
+	}
+
+	public void exibirTodasContas() {
+		for (Conta c : contas) {
+			c.imprimirInfosComuns();
+		}
+	}
+
+	public void apagarConta(int numero) {
+		Conta contaApagar = null;
+		if (!contas.isEmpty())
+			for (Conta c : contas) {
+				if (c.getNumero() == numero) {
+					contaApagar = c;
+					break;
+				}
+			}
+
+		if (contaApagar.getSaldo() == 0)
+			contas.remove(contaApagar);
+		else
+			System.out.println("Conta " + numero + " não pode ser removida, pois há saldo disponível!");
 	}
 
 }
