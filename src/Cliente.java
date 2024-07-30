cf7f3788955f38654374f2b8d00e665c3a25aa7f
WenFra005 <wendellfrancisco2005@hotmail.com>
2024-07-30 15:15:22 -0300

    Correção de bug

--- src/Cliente.java
+++ src/Cliente.java
@@ -42,7 +42,7 @@ public class Cliente {
 		if (cpf == null || cpf.length() != 11) {
 			throw new IllegalArgumentException("CPF inválido. Deve conter 11 dígitos");
 		}
-		return telefone.substring(0, 0) + "(" + telefone.substring(0, 2) + ") " + telefone.substring(2, 7) + " - " + telefone.substring(7, 11);
+		return cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9, 11);
 	}
 	
     private String formatarTelefone(String telefone) {
