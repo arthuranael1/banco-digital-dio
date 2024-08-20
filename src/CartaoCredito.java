c77e207d76aaf4ccc2ad0e4c5dcc232ace5fddd9
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-20 00:09:32 -0300

    add cartao credito

---
+++ src/CartaoCredito.java
@@ -0,0 +1,50 @@
+public public class CartaoDeCredito {
+    private String numeroCartao;
+    private String nomeTitular;
+    private String validade;
+    private double limite;
+    private double saldoDevedor;
+
+    public CartaoDeCredito(String numeroCartao, String nomeTitular, String validade, double limite) {
+        this.numeroCartao = numeroCartao;
+        this.nomeTitular = nomeTitular;
+        this.validade = validade;
+        this.limite = limite;
+        this.saldoDevedor = 0;
+    }
+
+    public void realizarCompra(double valor) {
+        if (valor <= limite - saldoDevedor) {
+            saldoDevedor += valor;
+        } else {
+            System.out.println("Limite insuficiente.");
+        }
+    }
+
+    public void pagarFatura(double valor) {
+        saldoDevedor -= valor;
+    }
+
+    public double getSaldoDevedor() {
+        return saldoDevedor;
+    }
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+}
+ CartaoCredito {
+    
+}
