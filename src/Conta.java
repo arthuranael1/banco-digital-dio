7b041032bda50ea714de245301089d21e92cbb8f
joaopedro0013 <joaopedro2002trabalho@gmail.com>
2024-10-28 21:46:55 -0300

    novo banco

--- src/Conta.java
+++ src/Conta.java
@@ -4,10 +4,22 @@ public abstract class Conta implements IConta {
 	private static final int AGENCIA_PADRAO = 1;
 	private static int SEQUENCIAL = 1;
 
+
 	protected int agencia;
+	protected int acaoImprestimo;
 	protected int numero;
 	protected double saldo;
 	protected Cliente cliente;
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	protected double valorImprestimos;
 
 	public Conta(Cliente cliente) {
 		this.agencia = Conta.AGENCIA_PADRAO;
@@ -30,6 +42,20 @@ public abstract class Conta implements IConta {
 		this.sacar(valor);
 		contaDestino.depositar(valor);
 	}
+	public void Imprestimo(double valor, int acaoImprestimo){
+		saldo += valor;
+		valorImprestimos += valor;
+
+		if(acaoImprestimo == 1){
+			saldo += valor;
+			System.out.println(String.format("Você tem 2 meses para pagar o imprestimo de " +  valor));
+		}else if(acaoImprestimo == 2){
+			saldo -= valor;
+			valorImprestimos -= valor;
+			System.out.println(String.format("Obrigado por pagar o imprestimo de " +  valor));
+		}
+
+	}
 
 	public int getAgencia() {
 		return agencia;
@@ -47,6 +73,7 @@ public abstract class Conta implements IConta {
 		System.out.println(String.format("Titular: %s", this.cliente.getNome()));
 		System.out.println(String.format("Agencia: %d", this.agencia));
 		System.out.println(String.format("Numero: %d", this.numero));
+		System.out.println(String.format("Valor de Emprestimos: %.2f", this.valorImprestimos));
 		System.out.println(String.format("Saldo: %.2f", this.saldo));
 	}
 }
